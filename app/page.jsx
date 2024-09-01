682164f74078a93f4c7cde04a5528d9bdaf7e20d
Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>
2024-09-01 17:51:28 +0800

    fix some errors

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,19 @@
 "use client";
 import styles from "./page.module.scss";
-import { useEffect, useState } from "react";
-import { AnimatePresence } from "framer-motion";
-import Preloader from "../components/Preloader";
-import Landing from "../components/Landing";
-import Projects from "../components/Projects";
-import Description from "../components/Description";
+import { useEffect } from "react";
+import Landing from "../components/landing/index";
+import Projects from "../components/Projects/index";
+import Description from "../components/Description/index";
 import SlidingImages from "../components/slidigImages/index";
-import Contact from "../components/Contact";
+import Contact from "../components/Contact/index";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false);
-
   useEffect(() => {
     (async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
       const locomotiveScroll = new LocomotiveScroll();
 
       setTimeout(() => {
-        setIsLoading(false);
         document.body.style.cursor = "default";
         window.scrollTo(0, 0);
       }, 2000);
@@ -26,9 +21,6 @@ export default function Home() {
   }, []);
   return (
     <main className={styles.main}>
-      <AnimatePresence mode="wait">
-        {isLoading && <Preloader />}
-      </AnimatePresence>
       <Landing />
       <Description />
       <Projects />
