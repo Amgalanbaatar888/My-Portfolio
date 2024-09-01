dd6614f4ac5dee657ce71fc9e3086095a0c7fe8a
Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>
2024-09-01 18:31:49 +0800

    add hi

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,35 @@
 "use client";
 import styles from "./page.module.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Landing from "../components/landing/index";
 import Projects from "../components/projects/Projects";
 import Description from "../components/description/Description";
 import SlidingImages from "../components/slidigImages/index";
 import Contact from "../components/contact/Contact";
+import Preloader from "../components/prelouder/Prelouder";
+import { AnimatePresence } from "framer-motion";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     (async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
       const locomotiveScroll = new LocomotiveScroll();
 
       setTimeout(() => {
+        setIsLoading(false);
         document.body.style.cursor = "default";
         window.scrollTo(0, 0);
       }, 2000);
     })();
   }, []);
+
   return (
     <main className={styles.main}>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Landing />
       <Description />
       <Projects />
